f1e2563c757d6626a5b4aa414318544c72cc3e33
Lucas Cavalcanti de Araujo <lcavalcantidearaujo@gmail.com>
2022-04-28 17:28:25 -0300

    Alteração no index.html e no script.js

--- script.js
+++ script.js
@@ -6,19 +6,17 @@ let lateral = document.querySelector(".d-1-right");
 let numeros = document.querySelector(".d-1-3");
 
  // variáveis de controle de ambiente:
- // para saber em que etapa atual está agora
-let etapaAtual = 0;   //para ser usada de parâmetro do array etapas
-let numero = "";      //números que ja foram preenchidos
+let etapaAtual = 0;
+let numero = "";
 let votoBranco = false;
-let votos = [];   //array onde vai ficar guardado os votos no final, para serem enviados
+let votos = [];
 
-//função que vai limpar a tela, pegar as informações da etapa atual e preencher a tela com elas
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
 
-    numero = ""; // pois sempre que executa essa função, o número que está na memória é zerado
-    votoBranco = ""; // pois sempre que executa essa função, essa variável precisa ser zerada
-    let numeroHtml = ""; // precisa montar essa variável de acordo com quantos números são para aquele cargo
+    numero = ""; 
+    votoBranco = "";
+    let numeroHtml = ""; 
     
     for(let i=0;i<etapa.numeros;i++){
         if (i === 0) {
@@ -36,27 +34,25 @@ function comecarEtapa() {
     numeros.innerHTML = numeroHtml;
 }
 
-// função que vai ser executada sempre que realizar uma ação (digitar um número)
 function atualizaInterface() {
     let etapa = etapas[etapaAtual];
-    let candidato = etapa.candidatos.filter( (item)=>{   //recebe cada um dos candidatos como parametro
+    let candidato = etapa.candidatos.filter( (item)=>{   
         if (item.numero === numero) {     //se o numero do candidato é igual ao número digitado
             return true;
         } else {
             return false;
         }
     } );
-    if (candidato.length > 0) {   // se conseguiu encontrar um candidato (pois se não encontra, retorna um array vazio, com 0)
-        candidato = candidato[0];  // pois como usou um filter, vau retornar um array com um só ítem para candidato (então pega o primeiro e único item)
-        //ou seja, candidato vai ser igual ao candidato de fato que foi encontrado
-        //ENCONTRADO ESSE CANDIDATO, VAMOS PREENCHER AS INFORMAÇÕES NA TELA:
+    if (candidato.length > 0) {   
+        candidato = candidato[0];  
+
         seuVotoPara.style.display = "block";
         aviso.style.display = "block";
         descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
-        //montando a estrutura das fotos para colocar em lateral:
+        
         let fotosHtml = ``;
-        for (let i in candidato.fotos){   //percorrer o array fotos dentro de candidato (pois alguns tem mais de uma foto)
-            if (candidato.fotos[i].small) {  //verificando se precisa colocar a class "small" na foto 
+        for (let i in candidato.fotos){   
+            if (candidato.fotos[i].small) {  
                 fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].src}" alt=""/>${candidato.fotos[i].legenda} </div>`
             } else{
                 fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].src}" alt=""/>${candidato.fotos[i].legenda} </div>`
@@ -65,25 +61,24 @@ function atualizaInterface() {
 
         lateral.innerHTML = fotosHtml;
 
-    } else {  // se não encontrou candidato com aquele número - VOTO NULO
+    } else {  // VOTO NULO
         seuVotoPara.style.display = "block";
         aviso.style.display = "block";
-        descricao.innerHTML = '<div class= "aviso--grande pisca">VOTO NULO</div>'  //cria-se um nova class (aviso-grande)
+        descricao.innerHTML = '<div class= "aviso--grande pisca">VOTO NULO</div>' 
     }
 }
 
 function clicou(n) {
-    //assim que clicar em um número, primeiramente se verifica se há algum quadrado com o pisca ativado
-    let elNumero = document.querySelector(".numero.pisca"); //seleciona o quadrado que tiver com pisca - pois é onde vai preencher
-    if (elNumero !== null) {   //ou seja, se há algum número piscando
-        elNumero.innerHTML = n; //preenchendo o quadrado piscando com o número digitado (n)
+    
+    let elNumero = document.querySelector(".numero.pisca"); 
+    if (elNumero !== null) {   
+        elNumero.innerHTML = n; 
         numero = `${numero}${n}`
 
-        //remover o pisca do que foi preenchido e passar para o próximo
         elNumero.classList.remove("pisca");
-        if (elNumero.nextElementSibling !== null) {  //verificando se há um número a seguir (pois o último número não tem ninguem depois)
-            elNumero.nextElementSibling.classList.add("pisca"); //adiciona no próximo
-        } else {  //ou seja, ja preencheu até o último item
+        if (elNumero.nextElementSibling !== null) {  
+            elNumero.nextElementSibling.classList.add("pisca"); 
+        } else {  
             atualizaInterface();
         }
     }
@@ -91,32 +86,31 @@ function clicou(n) {
 
 
 function branco(){
-    if (numero === "") {  //pois esse função só funciona se não tiver nada digitado (tem que apertar corrige antes)
+    if (numero === "") {  
         votoBranco = true;
         seuVotoPara.style.display = "block";
         aviso.style.display = "block";
-        numeros.innerHTML = "";  //vai remover os números
+        numeros.innerHTML = "";  
         descricao.innerHTML = '<div class= "aviso--grande pisca">VOTO EM BRANCO</div>'
     } else {
         alert("Para votar em BRANCO, não pode ter digitado nenhum número!")
     }
 }
 function corrige(){
-    comecarEtapa();        //pois esse botão apenas reinicia
+    comecarEtapa();        
 }
 function confirma(){
-    //o botão confirma só funciona se tiver preenchido algum número (mesmo que nulo) ou se tiver votado em branco
     let etapa = etapas[etapaAtual];
 
-    let votoConfirmado = false; //variável que confirma o voto
+    let votoConfirmado = false; 
 
-    if (votoBranco === true) {  //votou em branco
-        votoConfirmado = true; //pois o voto foi confirmado
-        votos.push({   //o que vai ser enviado
+    if (votoBranco === true) {  
+        votoConfirmado = true; 
+        votos.push({   
             etapa: etapas[etapaAtual].titulo,
             voto: "branco"
         });
-    } else if (numero.length === etapa.numeros) {  //deve ter digitado todos os números que tiverem nessa etapa
+    } else if (numero.length === etapa.numeros) {  
         votoConfirmado = true;
         votos.push({
             etapa: etapas[etapaAtual].titulo,
@@ -124,11 +118,11 @@ function confirma(){
         });
     }
 
-    if (votoConfirmado === true) {   //vai passar para a próxima etapa
+    if (votoConfirmado === true) {  
         etapaAtual++;
-        if (etapas[etapaAtual] !== undefined) { //se essa etapa existir (for diferente de "indefinida")
+        if (etapas[etapaAtual] !== undefined) { 
             comecarEtapa();
-        } else {      //não existem mais etapas, então quer dizer que a votação chegou ao fim (MOSTRAR A TELA FIM)
+        } else {    
             document.querySelector(".tela").innerHTML = '<div class= "aviso--gigante pisca">FIM</div>'
             console.log(votos);
         }
